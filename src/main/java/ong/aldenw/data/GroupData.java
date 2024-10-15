598001d9bda83127cc996d3666f2586fac46f06e
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-14 22:19:31 -0700

    update GroupData placeholders

--- src/main/java/ong/aldenw/data/GroupData.java
+++ src/main/java/ong/aldenw/data/GroupData.java
@@ -1,12 +1,14 @@
 package ong.aldenw.data;
 
+import ong.aldenw.util.RgbFormat;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class GroupData {
-    public String name;
-    public String prefix;
-    public int color;
+    public String name = "";
+    public String prefix = "";
+    public int color = RgbFormat.fromThree(255, 255, 255);
     public boolean listed = false;
     public boolean open = false;
     public UUID leader;
