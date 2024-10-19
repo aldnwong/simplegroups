2c545a054d7d7b4cea309aa73d83ac77a9dbf70f
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-18 20:43:38 -0700

    Add ability to get offline player's group

--- src/main/java/ong/aldenw/commands/GroupOfCommand.java
+++ src/main/java/ong/aldenw/commands/GroupOfCommand.java
@@ -2,18 +2,29 @@ package ong.aldenw.commands;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import ong.aldenw.GroupManager;
 import ong.aldenw.data.PlayerData;
+import ong.aldenw.util.RgbFormat;
+
+import java.util.Objects;
 
 public class GroupOfCommand {
     public static int execute(CommandContext<ServerCommandSource> context) {
         String playerArg = StringArgumentType.getString(context, "player");
-        ServerPlayerEntity player = context.getSource().getWorld().getServer().getPlayerManager().getPlayer(playerArg);
+        MinecraftServer server = context.getSource().getWorld().getServer();
+        GroupManager serverState = GroupManager.getServerState(server);
+        ServerPlayerEntity player = server.getPlayerManager().getPlayer(playerArg);
 
-        PlayerData playerState = GroupManager.getPlayerState(player);
+        if(!Objects.nonNull(player) && !serverState.playerUuidCache.containsKey(playerArg)) {
+            context.getSource().sendFeedback(() -> Text.literal("Player not found.").withColor(RgbFormat.DARK_RED), false);
+            return 1;
+        }
+
+        PlayerData playerState = (Objects.nonNull(player)) ? GroupManager.getPlayerState(player) : GroupManager.getPlayerState(serverState.playerUuidCache.get(playerArg), server);
 
         String result = !playerState.groupName.isEmpty() ? playerArg + " is in group " + playerState.groupName : playerArg + " is not in a group";
 
