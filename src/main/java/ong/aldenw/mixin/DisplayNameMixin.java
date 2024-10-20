a4a08802d35e761fed6b6e911594dbca3e780d81
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-20 16:53:35 -0700

    Remove unnecesary import

--- src/main/java/ong/aldenw/mixin/DisplayNameMixin.java
+++ src/main/java/ong/aldenw/mixin/DisplayNameMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import net.minecraft.client.MinecraftClient;
 
 import java.util.Objects;
 
