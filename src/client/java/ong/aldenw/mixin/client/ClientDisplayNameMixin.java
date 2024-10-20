9380c2cdd3aebcf30295afe04cd7190b94dfe486
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-20 16:52:08 -0700

    Fix color not showing when group doesn't have a prefix

--- src/client/java/ong/aldenw/mixin/client/ClientDisplayNameMixin.java
+++ src/client/java/ong/aldenw/mixin/client/ClientDisplayNameMixin.java
@@ -1,18 +1,46 @@
 package ong.aldenw.mixin.client;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.encryption.PublicPlayerSession;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
+import ong.aldenw.GroupManager;
+import ong.aldenw.data.GroupData;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Objects;
+import java.util.UUID;
+
 @Mixin(PlayerListEntry.class)
-public class ClientDisplayNameMixin {
+public abstract class ClientDisplayNameMixin {
+	@Shadow public abstract GameProfile getProfile();
+
 	@Inject(at = @At("RETURN"), method = "getDisplayName", cancellable = true)
 	private void injected(CallbackInfoReturnable<Text> cir) {
-		cir.setReturnValue(Text.literal("caw"));
+		MinecraftServer server = MinecraftClient.getInstance().world.getServer();
+		String playerName = this.getProfile().getName();
+
+		PlayerEntity player = (PlayerEntity)(Object)this;
+		GroupManager state = GroupManager.getServerState(Objects.requireNonNull(player.getServer()));
+		String groupName = GroupManager.getPlayerState(player).groupName;
+		GroupData groupState = state.groupList.get(groupName);
+		if(groupName.isEmpty()) {
+			cir.setReturnValue(Text.literal(playerName));
+		}
+		else if (groupState.prefix.isEmpty()) {
+			cir.setReturnValue(Text.literal(playerName).withColor(groupState.color));
+		}
+		else {
+			cir.setReturnValue(Text.empty().append(groupState.prefix).append(" ").append(playerName).withColor(groupState.color));
+		}
 	}
 }
--- src/main/java/ong/aldenw/mixin/DisplayNameMixin.java
+++ src/main/java/ong/aldenw/mixin/DisplayNameMixin.java
@@ -1,19 +1,15 @@
 package ong.aldenw.mixin;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.scoreboard.Team;
-import net.minecraft.text.ClickEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import ong.aldenw.GroupManager;
+import ong.aldenw.data.GroupData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.MinecraftClient;
 
 import java.util.Objects;
 
@@ -26,10 +22,15 @@ public abstract class DisplayNameMixin {
         PlayerEntity player = (PlayerEntity)(Object)this;
         GroupManager state = GroupManager.getServerState(Objects.requireNonNull(player.getServer()));
         String groupName = GroupManager.getPlayerState(player).groupName;
+        GroupData groupState = state.groupList.get(groupName);
         if(groupName.isEmpty()) {
             return Text.empty().append(this.getName());
-        } else {
-            return Text.empty().append(state.groupList.get(groupName).prefix).append(this.getName()).withColor(state.groupList.get(groupName).color);
+        }
+        else if (groupState.prefix.isEmpty()) {
+            return Text.empty().append(this.getName()).withColor(groupState.color);
+        }
+        else {
+            return Text.empty().append(groupState.prefix).append(" ").append(this.getName()).withColor(groupState.color);
         }
     }
 }
