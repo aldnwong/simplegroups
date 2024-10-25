cddf0d529c9b6bdbb9dec09e914c6c831352ce2e
Alden <31806638+aldnwong@users.noreply.github.com>
2024-10-25 00:26:09 -0700

    Add group request suggestion handler

--- src/main/java/ong/aldenw/commands/suggestions/RequestSuggestions.java
+++ src/main/java/ong/aldenw/commands/suggestions/RequestSuggestions.java
@@ -1,4 +1,37 @@
 package ong.aldenw.commands.suggestions;
 
-public class RequestSuggestions {
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import ong.aldenw.GroupManager;
+import ong.aldenw.data.GroupData;
+import ong.aldenw.data.PlayerData;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RequestSuggestions implements SuggestionProvider<ServerCommandSource> {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+        ServerCommandSource source = context.getSource();
+        MinecraftServer server = source.getServer();
+        GroupManager state = GroupManager.getServerState(server);
+        PlayerEntity player = source.getPlayer();
+        PlayerData playerData = GroupManager.getPlayerState(player);
+        GroupData groupData = state.groupList.get(playerData.groupName);
+
+        if (!source.isExecutedByPlayer() || playerData.groupName.isEmpty() || !player.getUuid().equals(groupData.leader)) return builder.buildFuture();
+
+        groupData.requests.forEach(uuid -> {
+            builder.suggest(uuid.toString());
+        });
+
+        builder.suggest("@all");
+
+        return builder.buildFuture();
+    }
 }
