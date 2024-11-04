bdf14d424cda99589c22c41964c7c18edde5704d
Alden <31806638+aldnwong@users.noreply.github.com>
2024-11-03 16:08:22 -0800

    Add RgbInt bounds

--- src/main/java/ong/aldenw/formats/RgbIntFormat.java
+++ src/main/java/ong/aldenw/formats/RgbIntFormat.java
@@ -1,8 +1,11 @@
 package ong.aldenw.formats;
 
 public class RgbIntFormat {
+    public static final int RGB_INT_MAX = 16777215;
+    public static final int RGB_INT_MIN = 0;
+
     public static int fromThree(int r, int g, int b) {
-        return (((r << 8) + g) << 8) + b;
+        return boundInt((((r << 8) + g) << 8) + b);
     }
 
     public static int fromHex(String hexCode) {
@@ -11,6 +14,17 @@ public class RgbIntFormat {
         String rHex = hexCode.substring(0, 2);
         String gHex = hexCode.substring(2, 4);
         String bHex = hexCode.substring(4, 6);
-        return (((Integer.parseInt(rHex, 16) << 8) + (Integer.parseInt(gHex, 16)) << 8) + (Integer.parseInt(bHex, 16)));
+
+        return boundInt((((Integer.parseInt(rHex, 16) << 8) + (Integer.parseInt(gHex, 16)) << 8) + (Integer.parseInt(bHex, 16))));
+    }
+
+    public static int boundInt(int color) {
+        if (color > RGB_INT_MAX)
+            color = RGB_INT_MAX;
+
+        if (color < RGB_INT_MIN)
+            color = RGB_INT_MIN;
+
+        return color;
     }
 }
